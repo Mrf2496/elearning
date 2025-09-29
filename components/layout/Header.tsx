8cceeede7439736b41d7c915814a0442105ca7be
Mrf2496 <111976580+Mrf2496@users.noreply.github.com>
2025-09-28 23:31:21 -0300

    feat: Simplify header display
    Removes the logout button and shortens the user display name in the header. This simplifies the header's visual presentation and prepares for a more centralized authentication management.

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import HomeIcon from '../icons/HomeIcon';
 import UserIcon from '../icons/UserIcon';
-import LogoutIcon from '../icons/LogoutIcon';
 
 const Header: React.FC = () => {
   return (
@@ -22,11 +21,7 @@ const Header: React.FC = () => {
         <div className="flex items-center space-x-4 text-sm">
           <div className="flex items-center space-x-2">
             <UserIcon className="w-5 h-5" />
-            <span className="font-semibold">Manuel Rodriguez</span>
-          </div>
-          <div className="flex items-center space-x-1.5 text-sky-200 hover:text-white cursor-pointer">
-            <LogoutIcon className="w-5 h-5" />
-            <span className="hidden sm:inline font-semibold">Salir</span>
+            <span className="font-semibold">MRF</span>
           </div>
         </div>
       </div>
